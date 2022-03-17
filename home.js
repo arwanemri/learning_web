4b216070c0b19eb35c70777ca129ef07423f4a63
arwanemri <101812792+arwanemri@users.noreply.github.com>
2022-03-17 22:24:28 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,30 @@
+
+const id = $(".id1");
+
+  const id2 = $(".h4");
+  id.hide()
+  id2.click(appear)
+function appear(){
+    id.show(1000)
+}
+
+
+const ed = $(".ed");
+
+  const ed1= $(".ed1");
+  ed1.hide()
+  ed.click(appears)
+function appears(){
+    ed1.show(1000)
+}
+
+const sk = $(".sk");
+
+  const sk1 = $(".sk1");
+  sk1.hide()
+  sk.click(appearss)
+function appearss(){
+    sk1.show(1000)
+}
+
+
